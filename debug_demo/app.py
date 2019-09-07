d6d9b5673ae0a5238393c891f347aad3487e0329
童 <tong@tongdeMacBook-Pro.local>
2019-09-08 00:13:46 +0800

    debug_demo

---
+++ debug_demo/app.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*- （代码中有中文需用UTF-8来翻译）
+
+sites = ["Baidu", "Google","Runoob","Taobao"]
+
+#将断点设于6处debug将一步一步执行循环（逐步输出Baidu,Google,Runoob,Taobao)
+for site in sites
+    print("循环数据 " + site)
+
+print("完成循环!")
